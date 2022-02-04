4f9734c8db93c003066f382f2570fba8ba665d72
nar2p1nk <farrisisqandar@gmail.com>
2022-02-04 16:42:24 +0800

    fixed bug where ejs doesn't render, added css and data.json

--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
 import express from "express";
-
+const ejs = require('ejs')
 const app: express.Application = express();
+const data = require('./data.json').todos;
+const port:number = 8080;
 
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+app.set('view-engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs',{data:data})
+});
+var todos:string;
+for(todos in data){console.log(data[todos].todo);
+}
+
+app.listen(port,()=>{console.log(`TypeScript with Express at https://localhost:${port}/`)
+;})
