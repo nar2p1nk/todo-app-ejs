5c3863871f20c76d76fbf32e30f67b1912c7ec37
nar2p1nk <farrisisqandar@gmail.com>
2022-02-05 00:04:45 +0800

    completed

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import sqlite3 from "sqlite3";
 const ejs = require('ejs')
 const app: express.Application = express();
-const data = require('./data.json').todos;
+const db = new sqlite3.Database('database.db',(err)=>{
+    if(err){console.error(err.message)}})
 const port:number = 8080;
 
 app.use(express.static('public'))
@@ -9,11 +11,31 @@ app.use(express.urlencoded({extended:false}))
 app.set('view-engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.render('index.ejs',{data:data})
+    db.all('SELECT * FROM todos',(err,row)=>{
+
+    
+    res.render('index.ejs',{data:row})
+})
 });
-var todos:string;
-for(todos in data){console.log(data[todos].todo);
-}
+
+app.post('/',(req,res)=>{
+    const PTodo:any = req.body.todo
+    console.log(PTodo);
+    db.run('INSERT INTO todos(todo) VALUES(?)',[PTodo])
+    res.redirect('/')
+})
+
+app.post('/check',(req,res)=>{
+    const ids:any = req.body.todo
+    var number:any;
+    for(number in ids){
+        db.run(`DELETE FROM todos WHERE id is ?`,[ids[number]],(err)=>{
+            
+        })
+        console.log(`deleted row ${ids[number]}`);
+    }
+    res.redirect('/')
+})
 
 app.listen(port,()=>{console.log(`TypeScript with Express at https://localhost:${port}/`)
 ;})
